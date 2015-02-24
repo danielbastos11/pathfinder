fceee3b71d92a889c8051aed830b0e7dea1f968d
Daniel Bastos <danielbastos@live.com>
2015-02-24 12:28:38 -0300

    Arrumação da apresentação

--- index.js
+++ index.js
@@ -21,11 +21,46 @@ head.load(
 					$('#go').click(function(){
 						var input = $('#input').val();
 						var maze = Renderer.render( input );
-
-						console.log( maze.graph );
 						var path = PathFinder.aStar( maze.graph, maze.initial, maze.goal);
 
-						console.log( path );
+
+						var output = '';
+
+						maze.array.forEach(function(row){
+							row.forEach(function(col){
+								var point;
+
+								switch( col.type ){
+									case 'path':
+										point = "0";
+										break;
+									case 'wall':
+										point = '1';
+										break;
+									case 'start':
+										point = '2';
+										break;
+									case 'exit':
+										point = '3';
+										break;
+								}
+
+								if( path.indexOf( col ) != -1 ){
+									var newPoint = "<span style='color: red'>";
+									newPoint += point;
+									newPoint += "</span>";
+
+									point = newPoint;
+								}
+
+								output += point;
+							});
+
+							output += '<br />';
+						});
+
+						$('#output').append( output );
+
 					})
 				})
 			}
